e7b0487b92b65408c485a7d25b89a64c9fb4fc89
Jose Molina <joseemolina@gmail.com>
2016-09-09 13:11:08 +0200

    Updated to work with spring-boot 1.4.0-RELEASE and spring-session-1.2.2. Fixed problem with embedded tomcat strict cookie checking (spring session problem https://github.com/spring-projects/spring-boot/issues/6827) Fixed Object to double conversion

--- src/test/java/com/github/mkopylec/sessioncouchbase/TestApplication.java
+++ src/test/java/com/github/mkopylec/sessioncouchbase/TestApplication.java
@@ -1,11 +1,13 @@
 package com.github.mkopylec.sessioncouchbase;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 import static org.springframework.boot.SpringApplication.run;
 
 @EnableCouchbaseHttpSession
 @SpringBootApplication
+@Import(TestConfiguration.class)
 public class TestApplication {
 
     public static void main(String[] args) {
