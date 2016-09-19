082a18c051be113fb865061faa6be9af79956011
Mariusz Kopylec <mariusz.kopylec@allegrogroup.com>
2016-09-19 09:35:33 +0200

    Timestamp casting fixed

--- src/main/java/com/github/mkopylec/sessioncouchbase/persistent/CouchbaseSession.java
+++ src/main/java/com/github/mkopylec/sessioncouchbase/persistent/CouchbaseSession.java
@@ -9,7 +9,6 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-import static java.lang.Math.round;
 import static java.lang.System.currentTimeMillis;
 import static java.util.Collections.unmodifiableSet;
 import static java.util.UUID.randomUUID;
@@ -60,12 +59,12 @@ public class CouchbaseSession implements ExpiringSession, Serializable {
 
     @Override
     public long getCreationTime() {
-        return ((Number)globalAttributes.get(CREATION_TIME_ATTRIBUTE)).longValue();
+        return ((Number) globalAttributes.get(CREATION_TIME_ATTRIBUTE)).longValue();
     }
 
     @Override
     public long getLastAccessedTime() {
-        return ((Number)globalAttributes.get(LAST_ACCESSED_TIME_ATTRIBUTE)).longValue();
+        return ((Number) globalAttributes.get(LAST_ACCESSED_TIME_ATTRIBUTE)).longValue();
     }
 
     public void setLastAccessedTime(long lastAccessedTime) {
--- src/test/java/com/github/mkopylec/sessioncouchbase/TestApplication.java
+++ src/test/java/com/github/mkopylec/sessioncouchbase/TestApplication.java
@@ -1,16 +1,40 @@
 package com.github.mkopylec.sessioncouchbase;
 
+import org.apache.catalina.Context;
+import org.apache.tomcat.util.http.LegacyCookieProcessor;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatContextCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 
 import static org.springframework.boot.SpringApplication.run;
 
 @EnableCouchbaseHttpSession
 @SpringBootApplication
-@Import(TestConfiguration.class)
 public class TestApplication {
 
     public static void main(String[] args) {
         run(TestApplication.class, args);
+    }
+
+    @Bean
+    public EmbeddedServletContainerCustomizer customizer() {
+        return new EmbeddedServletContainerCustomizer() {
+
+            @Override
+            public void customize(ConfigurableEmbeddedServletContainer container) {
+                TomcatEmbeddedServletContainerFactory tomcat = (TomcatEmbeddedServletContainerFactory) container;
+                TomcatContextCustomizer contextCustomizer = new TomcatContextCustomizer() {
+
+                    @Override
+                    public void customize(Context context) {
+                        context.setCookieProcessor(new LegacyCookieProcessor());
+                    }
+                };
+                tomcat.addContextCustomizers(contextCustomizer);
+            }
+        };
     }
 }
