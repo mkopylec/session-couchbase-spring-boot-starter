32f8d26d934d1946c99208db94c7b37ad95f42f7
Mariusz Kopylec <mariusz.kopylec@allegrogroup.com>
2016-10-14 16:03:52 +0200

    JsonDiff tests

--- src/test/java/com/github/mkopylec/sessioncouchbase/TestApplication.java
+++ src/test/java/com/github/mkopylec/sessioncouchbase/TestApplication.java
@@ -1,13 +1,63 @@
 package com.github.mkopylec.sessioncouchbase;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import foodev.jsondiff.Jackson2Diff;
+import foodev.jsondiff.JsonDiff;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
 
 import static org.springframework.boot.SpringApplication.run;
 
 @SpringBootApplication
 public class TestApplication {
 
-    public static void main(String[] args) {
+    public static class Test {
+
+        private int i;
+        private String s;
+        private Date d;
+
+        public int getI() {
+            return i;
+        }
+
+        public void setI(int i) {
+            this.i = i;
+        }
+
+        public String getS() {
+            return s;
+        }
+
+        public void setS(String s) {
+            this.s = s;
+        }
+
+        public Date getD() {
+            return d;
+        }
+
+        public void setD(Date d) {
+            this.d = d;
+        }
+    }
+
+    public static void main(String[] args) throws JsonProcessingException, InterruptedException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Test t = new Test();
+        t.setI(10);
+        t.setS("10");
+        t.setD(new Date());
+        Test t2 = new Test();
+        Thread.sleep(1000);
+        t2.setI(10);
+        t2.setS("20");
+        t2.setD(new Date());
+        JsonDiff jsonDiff = new Jackson2Diff();
+        String diff = jsonDiff.diff(objectMapper.writeValueAsString(t), objectMapper.writeValueAsString(t2));
+
         run(TestApplication.class, args);
     }
 
