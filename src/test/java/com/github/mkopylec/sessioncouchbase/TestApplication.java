dcaf51e2025919f9dc0d783dbe23e36224de18f8
Mariusz Kopylec <mariusz.kopylec@o2.pl>
2016-11-02 21:03:57 +0100

    indeterministic test debug 2

--- src/test/java/com/github/mkopylec/sessioncouchbase/TestApplication.java
+++ src/test/java/com/github/mkopylec/sessioncouchbase/TestApplication.java
@@ -1,7 +1,12 @@
 package com.github.mkopylec.sessioncouchbase;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import org.apache.tomcat.util.http.LegacyCookieProcessor;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatContextCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 
 import static org.springframework.boot.SpringApplication.run;
 
@@ -10,5 +15,14 @@ public class TestApplication {
 
     public static void main(String[] args) throws JsonProcessingException, InterruptedException {
         run(TestApplication.class, args);
+    }
+
+    @Bean
+    public EmbeddedServletContainerCustomizer customizer() {
+        return container -> {
+            TomcatEmbeddedServletContainerFactory tomcat = (TomcatEmbeddedServletContainerFactory) container;
+            TomcatContextCustomizer contextCustomizer = context -> context.setCookieProcessor(new LegacyCookieProcessor());
+            tomcat.addContextCustomizers(contextCustomizer);
+        };
     }
 }
